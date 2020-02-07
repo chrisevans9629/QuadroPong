0b299d73ae6316ffcbc89a386e7b0c3fd3794b26
Chris Evans <chrisevans9629@gmail.com>
2020-02-07 15:55:50 -0600

    refactored

---
+++ ParticleEngine/ParticleEngine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyGame
+{
+    public class ParticleEngine
+    {
+        private IRandomizer random;
+        public Vector2 EmitterLocation { get; set; }
+        private List<Particle> particles;
+        private List<Texture2D> textures;
+
+        public ParticleEngine(List<Texture2D> textures, Vector2 location, IRandomizer random)
+        {
+            EmitterLocation = location;
+            this.textures = textures;
+            this.particles = new List<Particle>();
+            this.random = random;
+        }
+
+        public void Update()
+        {
+            int total = 10;
+
+            for (int i = 0; i < total; i++)
+            {
+                particles.Add(GenerateNewParticle());
+            }
+
+            for (int particle = 0; particle < particles.Count; particle++)
+            {
+                particles[particle].Update();
+                if (particles[particle].TTL <= 0)
+                {
+                    particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                1f * (float)(random.NextFloat() * 2 - 1),
+                1f * (float)(random.NextFloat() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(random.NextFloat() * 2 - 1);
+            Color color = new Color(
+                (float)random.NextFloat(),
+                (float)random.NextFloat(),
+                (float)random.NextFloat());
+            float size = (float)random.NextFloat();
+            int ttl = 20 + random.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int index = 0; index < particles.Count; index++)
+            {
+                particles[index].Draw(spriteBatch);
+            }
+        }
+    }
+}
---
+++ Sprites/Collider.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.Xna.Framework;
+
+namespace MyGame
+{
+    public abstract class Collider
+    {
+        public abstract Rectangle Bounds();
+
+        private  List<Sprite> lastCollisions = new List<Sprite>();
+
+
+        public bool BetweenX(Sprite sprite)
+        {
+            var bounds = Bounds();
+            return sprite.Position.X >= bounds.X && sprite.Position.X + sprite.EndPoint.X <= bounds.X + bounds.Width;
+        }
+
+        public bool BetweenY(Sprite sprite)
+        {
+            var bounds = Bounds();
+            return sprite.Position.Y >= bounds.Y && sprite.Position.Y + sprite.EndPoint.Y <= bounds.Y + bounds.Height;
+        }
+
+        public Direction OnRelativeSide(Sprite sprite)
+        {
+            var bounds = Bounds();
+            if (BetweenX(sprite))
+            {
+                if (sprite.Position.Y >= bounds.Y + bounds.Height)
+                {
+                    return Direction.Bottom;
+                }
+                else
+                {
+                    return Direction.Top;
+                }
+            }
+            else if (BetweenY(sprite))
+            {
+                if (sprite.Position.X + sprite.Texture2D.Width >= bounds.X + bounds.Width)
+                {
+                    return Direction.Right;
+                }
+                else
+                {
+                    return Direction.Left;
+                }
+            }
+
+            return Direction.Bottom;
+        }
+
+        public bool Collision(Sprite sprite)
+        {
+            // Texture2D.Bounds.Intersects(sprite.Texture2D.Bounds);
+            var isColliding = Bounds().Intersects(sprite.Bounds());
+            if (isColliding)
+            {
+                if (lastCollisions.Contains(sprite))
+                {
+                    return false;
+                }
+
+                lastCollisions.Add(sprite);
+            }
+            else
+            {
+                lastCollisions.Remove(sprite);
+            }
+
+            return isColliding;
+        }
+    }
+}
