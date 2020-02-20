98c9be3a03308148fe79031f72e5c727254452c5
Chris Evans <chrisevans9629@gmail.com>
2020-02-19 18:54:19 -0600

    addeding signal r unit tests

---
+++ Tests/ServerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using Server;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ServerTests
+    {
+        [Test]
+        public void Test()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseTestServer();
+        }
+    }
+}
